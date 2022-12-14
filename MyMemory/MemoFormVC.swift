a3c23ae82cc2c55d830c3c872e552d26fd3b5430
nakrlove <nakrlove@gmail.com>
2022-12-14 19:44:50 +0900

    Chapter03 실습함

--- MyMemory/MemoFormVC.swift
+++ MyMemory/MemoFormVC.swift
@@ -18,14 +18,31 @@ class MemoFormVC: UIViewController, UIImagePickerControllerDelegate , UINavigati
         super.viewDidLoad()
         self.contents.delegate = self
         // Do any additional setup after loading the view.
+        let bgImage = UIImage(named: "memo-background.png")!
+        self.view.backgroundColor = UIColor(patternImage: bgImage)
+        
+        self.contents.layer.borderWidth = 0
+        self.contents.layer.borderColor = UIColor.clear.cgColor
+        self.contents.backgroundColor = UIColor.clear
+        
+        let style = NSMutableParagraphStyle()
+        style.lineSpacing = 9
+        self.contents.attributedText = NSAttributedString(string: " ", attributes: [.paragraphStyle: style])
+        self.contents.text = ""
+        
     }
 
 
     @IBAction func save(_ sender: Any) {
         
+        let alertV = UIViewController()
+        let iconImage = UIImage(named: "warning-icon-60")
+        alertV.view = UIImageView(image: iconImage)
+        alertV.preferredContentSize = iconImage?.size ?? CGSize.zero
+        
         guard self.contents.text?.isEmpty == false else {
-            
             let alert = UIAlertController(title: nil, message: "내용을 입력해주세요!", preferredStyle: .alert)
+            alert.setValue(alertV, forKey: "contentViewController")
             alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
             self.present(alert ,animated: true)
             return
@@ -64,5 +81,16 @@ class MemoFormVC: UIViewController, UIImagePickerControllerDelegate , UINavigati
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         self.preview.image = info[.editedImage] as? UIImage
         picker.dismiss(animated: false)
+    }
+    
+    
+    //네비게이션 바의 토클 처리를 하고 있습니다.
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let bar = self.navigationController?.navigationBar
+        
+        let ts = TimeInterval(0.3)
+        UIView.animate(withDuration: ts){
+            bar?.alpha = ( bar?.alpha == 0 ? 1: 0 )
+        }
     }
 }
